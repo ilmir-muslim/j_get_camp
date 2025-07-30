02a1062a75c497fe0f748288bb552de54203d6f1
ilmir <ilmir_gf@mail.ru>
2025-07-30 10:58:05 +0300

    feat: Обновить разрешенные хосты для настройки Django

--- jget_crm/settings.py
+++ jget_crm/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["jgetcrm.site.ru"]
+ALLOWED_HOSTS = ["j_get_camp.beget.tech"]
 
 
 # Application definition
