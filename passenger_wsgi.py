89166d5e3f2ce9ba619a735a2c78f9a691938e66
ilmir <ilmir_gf@mail.ru>
2025-07-30 16:13:03 +0300

    feat: Обновить конфигурацию .htaccess и passenger_wsgi.py для улучшения обработки WSGI и статических файлов

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,10 @@
-# -*- coding: utf-8 -*-
-import os, sys
-sys.path.insert(0, '/home/r/rusla9m5/j_get_camp')
-sys.path.insert(1, '/home/r/rusla9m5/j_get_camp/venv/lib/python3.11/site-packages/django/__init__.py')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'jget_crm.settings'
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+import os, sys
+activate_this = '/home/r/rusla9m5/j_get_camp/venv/bin/activate_this.py'
+with open(activate_this) as f:
+    exec(f.read(), {'__file__': activate_this})
+sys.path.insert(0, '/home/r/rusla9m5/j_get_camp')
+sys.path.insert(1, '/home/r/rusla9m5/j_get_camp/venv/lib/python3.11/site-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'jget_crm.settings'
+application = StaticFilesHandler(get_wsgi_application())
