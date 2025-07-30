fb453cabfdc0ba3f6e882ecb0f5029cb98f72efa
ilmir <ilmir_gf@mail.ru>
2025-07-30 15:08:40 +0300

    feat: Упрощение конфигурации .htaccess и passenger_wsgi.py для улучшения обработки запросов

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,7 @@
-import sys
-import os
-
-# Путь до директории, где manage.py
-project_path = "/home/venvrusla9m5/j_get_camp"
-if project_path not in sys.path:
-    sys.path.insert(0, project_path)
-
-os.environ["DJANGO_SETTINGS_MODULE"] = "jget_crm.settings"
-
+# -*- coding: utf-8 -*-
+import os, sys
+sys.path.insert(0, '/home/r/rusla9m5/j_get_camp')
+sys.path.insert(1, '/home/r/rusla9m5/j_get_camp/venv/lib/python3.11/site-packages/django/__init__.py')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'jget_crm.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
